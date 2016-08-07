67c10fa7b28a7366038964077a4069991149424d
Cliff Allderige <ca.allderige@gmail.com>
2016-08-06 21:17:09 -0400

    Added register and login functionality.

--- imports/components/entry/entry.js
+++ imports/components/entry/entry.js
@@ -7,8 +7,40 @@ Template.Entry.events({
 	},
 	"submit .registerUser": function(event,template) {
 		event.preventDefault();
+		let registerUser = template.find("#registerUser").value;
+		let registerPassword = template.find("#registerPassword").value;
+		let confirmPassword = template.find("#confirmPassword").value;
+
+		if(registerPassword == confirmPassword) {
+			Accounts.createUser({
+				username: registerUser,
+				password: registerPassword
+			});
+
+			Meteor.loginWithPassword(registerUser, registerPassword , function(err) { 
+				if (err) {
+					window.alert("Incorrect information, please try again");
+				} else {
+					let modal = document.getElementById('entry');
+					document.getElementById("entryModal").style.display = "none";	
+				}
+			});
+		} else {
+			window.alert("Passwords must match");
+		}
 	},
 	"submit .loginUser": function(event, template) {
 		event.preventDefault();
+		let user = template.find("#user").value;
+		let password = template.find("#password").value;
+
+		Meteor.loginWithPassword(user, password , function(err) { 
+				if (err) {
+					window.alert("Incorrect information, please try again");
+				} else {
+					let modal = document.getElementById('entry');
+					document.getElementById("entryModal").style.display = "none";	
+				}
+			});
 	}
 })
--- imports/components/nav/nav.js
+++ imports/components/nav/nav.js
@@ -1,9 +1,18 @@
 Template.Nav.events({
-	"submit .navSearch": function(event,template) {
+	"submit .navSearch": function(event, template) {
 		event.preventDefault();
 		let search = template.find("#spellname").value;
 	},
-	"click #login": function(event,template) {
+	"click #login": function(event, template) {
 		document.getElementById("entryModal").style.display = "block";
+	},
+	"click #logout": function(event, template) {
+		event.preventDefault();
+
+		Meteor.logout(function(err) {
+			if(err) {
+				window.alert("An error occured while logging out.")
+			}
+		});
 	}
 })
